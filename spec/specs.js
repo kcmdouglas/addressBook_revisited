71d2f91ff0e85c3a0346b6c7f9deec0494f680fd
Gregory K and Nathan Romike <nathanromike@gmail.com>
2016-02-08 11:48:59 -0800

    Add fullName method

--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,13 @@
 describe('Contact', function() {
+  var testContact = new Contact("Anonymous", "Personage");
+
   it('creates a new contact with provided properties', function() {
-    var testContact = new Contact("Anonymous", "Personage");
     expect(testContact.firstName).to.equal("Anonymous");
     expect(testContact.lastName).to.equal("Personage");
     expect(testContact.addresses).to.eql([]);
   });
 });
+
+  it('adds the fullName method to all contacts', function() {
+    expect(testContact.fullName()).to.equal('Anonymous Personage');
+  });
