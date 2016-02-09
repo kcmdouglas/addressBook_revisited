bdd56df3704bf9e9f1cfc376feb31bad0113f264
Gregory K and Kassidy Douglas <student@epicodus.com>
2016-02-09 10:00:58 -0800

    Add JQuery fade-in

--- js/scripts.js
+++ js/scripts.js
@@ -73,7 +73,7 @@ $(document).ready(function() {
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $(".contact").last().click(function() {
-      $("#show-contact").show();
+      $("#show-contact").fadeIn(3000);
       $("#show-contact h2").text(newContact.fullName());
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
